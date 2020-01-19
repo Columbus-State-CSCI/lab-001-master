df3d17f1e8132bb7b1f5ca183d55e8afe9fb8a7b
John Crider <jcrider4@cscc.edu>
2020-01-19 01:26:38 -0800

    add a comment

---
+++ lab1f/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1f
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initial COde commit
+        }
+    }
+}
